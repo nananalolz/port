9367f96a47ec8615057b5fdf630b19699fb4cdd4
nananalolz <95423377+nananalolz@users.noreply.github.com>
2022-01-19 23:41:21 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+
+  //ページ内スクロール
+  var navHeight = $(".gnav").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
+
+});
